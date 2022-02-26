ee1e0d431767ccd2e4e69b627173aecb3b78ea73
Chirag Jain <jainchg@gmail.com>
2022-02-27 02:00:03 +0530

    Add activity resuly handler

--- app/src/main/java/com/chiragjn/template/activity/BaseActivity.java
+++ app/src/main/java/com/chiragjn/template/activity/BaseActivity.java
@@ -1,8 +1,15 @@
 package com.chiragjn.template.activity;
 
+import android.content.Intent;
+
+import androidx.activity.result.ActivityResult;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.chiragjn.template.utils.ActivityResultHandler;
+
 abstract public class BaseActivity extends AppCompatActivity {
+
+    protected final ActivityResultHandler<Intent, ActivityResult> activityLauncher = ActivityResultHandler.registerActivityForResult(this);
 
     abstract void bindView();
 }
--- app/src/main/java/com/chiragjn/template/activity/MainActivity.java
+++ app/src/main/java/com/chiragjn/template/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.chiragjn.template.activity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,9 +12,13 @@ public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
 
+    private Context ctx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ctx = this;
 
         bindView();
     }
@@ -21,6 +28,20 @@ public class MainActivity extends BaseActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
+        View view = binding.getRoot();
+        setContentView(view);
+
         binding.helloWorld.setVisibility(View.VISIBLE);
+    }
+
+    void startNewActivity() {
+        // Specify the new activity
+        Intent intent = new Intent(ctx, MainActivity.class);
+        activityLauncher.launch(intent, result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                // do something with the data
+            }
+        });
     }
 }
